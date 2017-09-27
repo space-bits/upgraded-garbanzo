8673c39eaea075d8caedaeaa1ed1b52010ede78c
Rob <rlainewyllie@gmail.com>
2017-09-28 01:08:38 +1000

    Add bikestation controller

--- routes/web.php
+++ routes/web.php
@@ -6,20 +6,5 @@ Route::get('/', function () {
     return view('landingPage');
 });
 
-Route::get('/bikeStations', function() {
-    // Defined in BikeStation model
-    $bikeStations = BikeStation::all();
-
-    return view('bikeStations.index', compact('bikeStations'));
-});
-
-Route::get('/bikeStations/{bikeStation}', function($id) {
-
-    $bikeStation = BikeStation::find($id);
-
-    if($bikeStation != null){
-        return view('bikeStations.show', compact('bikeStation'));
-    } else {
-        // Some error page resource not found or something
-    }
-});
+Route::get('/bikeStations', 'BikeStationsController@index');
+Route::get('/bikeStations/{id}', 'BikeStationsController@show');
