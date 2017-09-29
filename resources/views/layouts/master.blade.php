2ae72033e96a63a871195a5a9f8cf36937433ba5
Rob <rlainewyllie@gmail.com>
2017-09-30 02:16:42 +1000

    Add bootstrap and seperate template into files

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,29 +1,31 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
-  <head>
+<head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-    <!-- <link rel="icon" href="../../favicon.ico"> -->
+    <link rel="icon" href="../../favicon.ico">
 
-    <title>Jumbotron Template for Bootstrap</title>
+    <title>Upgraded Grabanzo - There is no Garbanzo.</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
 
     <!-- Custom styles for this template -->
     <link href="app.css" rel="stylesheet">
-  </head>
-  <body>
-      @include('layouts.nav')
+</head>
+<body>
+    @include('layouts.nav')
 
-      <div class="content container">
-          @yield('content')
-      </div>
+    <div class="content container">
+        @yield('content')
+    </div>
 
-      @include('layouts.sidebar')
-      @include('layouts.footer')
-  </body>
-  </html>
+    @include('layouts.sidebar')
+    @include('layouts.footer')
+</body>
+</html>
