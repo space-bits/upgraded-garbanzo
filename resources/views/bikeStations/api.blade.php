f1aa379bf736f0b792264d8357a33461c7c4d861
Rob <rlainewyllie@gmail.com>
2017-10-05 16:07:14 +1100

    Include navbar in bikes/api and remove old navbar
        - Unify all navbars accross all files
        - Remove search from navbar; Unrequired

--- resources/views/bikeStations/api.blade.php
+++ resources/views/bikeStations/api.blade.php
@@ -4,6 +4,8 @@
 
 <div class="container-fluid">
     <div class="row">
+        @include('layouts.nav')
+<!--
         <nav class="col-sm-3 col-md-2 hidden-xs-down bg-faded sidebar">
             <ul class="nav nav-pills flex-column">
                 <li class="nav-item">
@@ -19,7 +21,7 @@
                     <a class="nav-link" href="#">Export</a>
                 </li>
             </ul>
-        </nav>
+        </nav> -->
 
         <main class="col-sm-9 offset-sm-3 col-md-10 offset-md-2 pt-3">
             <h1>Dashboard</h1>
--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -3,15 +3,16 @@
     <div class="navbar-collapse" id="navbars">
       <ul class="navbar-nav mr-auto">
         <li class="nav-item">
-            <a class="nav-link" href='{!! url('/stations/api'); !!}'>Bikes</a>
+            <a class="nav-link" href='{!! url('/bikes/api'); !!}'>Bikes</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href='{!! url('/pedestrians/api'); !!}'>Pedestrians</a>
         </li>
       </ul>
-      <form class="form-inline mt-2 mt-md-0">
+      <!-- Search bar; Not required but keep as a contingency -->
+      <!-- <form class="form-inline mt-2 mt-md-0">
         <input class="form-control mr-sm-2" type="text" placeholder="Search">
         <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-      </form>
+      </form> -->
     </div>
 </nav>
