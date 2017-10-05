03fb516cfe129e085149f988129d3057d402b3e6
Rob <rlainewyllie@gmail.com>
2017-10-05 12:35:18 +1100

    Use artisan to make skeletons of unit and feature tests for bike stuff

---
+++ tests/Unit/BikeTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class BikeTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testSchema()
+    {
+        
+    }
+}
--- vendor/composer/autoload_classmap.php
+++ vendor/composer/autoload_classmap.php
@@ -30,7 +30,7 @@ return array(
     'App\\Providers\\EventServiceProvider' => $baseDir . '/app/Providers/EventServiceProvider.php',
     'App\\Providers\\RouteServiceProvider' => $baseDir . '/app/Providers/RouteServiceProvider.php',
     'App\\User' => $baseDir . '/app/User.php',
-    'BikeStationSeeder' => $baseDir . '/database/seeds/DatabaseSeeder.php',
+    'BikeStationSeeder' => $baseDir . '/database/seeds/BikeStationSeeder.php',
     'Carbon\\Carbon' => $vendorDir . '/nesbot/carbon/src/Carbon/Carbon.php',
     'Carbon\\CarbonInterval' => $vendorDir . '/nesbot/carbon/src/Carbon/CarbonInterval.php',
     'Carbon\\Exceptions\\InvalidDateException' => $vendorDir . '/nesbot/carbon/src/Carbon/Exceptions/InvalidDateException.php',
