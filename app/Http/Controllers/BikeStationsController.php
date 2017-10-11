5dde57028d1c00663a7bae739137c6fd30d35039
Rob <rlainewyllie@gmail.com>
2017-10-11 17:07:13 +1100

    add year parameters to api query

--- app/Http/Controllers/BikeStationsController.php
+++ app/Http/Controllers/BikeStationsController.php
@@ -108,21 +108,15 @@ class BikeStationsController extends Controller
     public function getOpenBikeData()
     {
         $bikeStations = array();
-        
+
         //https://stackoverflow.com/questions/6516902/how-to-get-response-using-curl-in-php#6518125
         $base_url = 'https://data.melbourne.vic.gov.au/resource/';
         $bike_url = 'qnjw-wgaj.json';
 
-        $year;
+        $year = $_GET['year'];
         $day;
 
-        // Pass day and year paramets from the view and then modify the api call
-        // Create a different query based on the forms
-        if(!empty($year) || !empty($day)) {
-
-        } else {
-            $url = $base_url.$bike_url;
-        }
+        $url = $base_url.$bike_url."?year=".$year;
 
         $options = array(
             CURLOPT_RETURNTRANSFER => true,   // return web page
