87d9a5b7cf1a4d8443feec1a9949f463b5343057
Gabriel <gabrielhaines@hotmail.com>
2017-10-12 23:39:46 +1100

    U nerd

--- resources/views/bikeStations/api.blade.php
+++ resources/views/bikeStations/api.blade.php
@@ -72,7 +72,8 @@
                  Form::close() !!}
 
             </div> -->
-
+            <div id="chartContainer" style="height: 370px; width: 100%;"></div>
+            <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
             <div class="table-responsive">
                 <table class="table table-striped">
                     <thead>
